afbbf799d706379998dec4d91194cc457b497344
silif <1054927886@qq.com>
2018-04-16 17:21:40 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,84 @@
-// 开发时 gulp watch 编译scss，js/css修改后自动刷新浏览器
-// 上线前 gulp build img压缩，js压缩打包，css压缩打包，给 static files 添加版本号
-// 警告： 在 build 之前必须先 watch
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
 var rev = require('gulp-rev');
 var runSequence = require('run-sequence');
 var revCollector = require('gulp-rev-collector');
-
+var htmlmin = require('gulp-htmlmin');
+var del = require('del');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var uglifycss = require('gulp-uglifycss');
 var uglify = require('gulp-uglify');
+var plugins = [
+    autoprefixer({browsers: ['last 1 version']}),
+    cssnano()
+];
 
-// autoprefixer设置
-var autoprefixerOptions = {
-    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-};
+// process JS files and return the stream.
+gulp.task('serve', function() {
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.+(scss|css)')
-        .pipe(sass())
-        .pipe(concat('all.css'))
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
-gulp.task('comcatjs', function() {
-    return gulp.src('app/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('app/js/build'));
-});
-// 浏览器自动刷新配置
-gulp.task('browserSync', function() {
     browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
+        server: "./src"
+    });
+
+    gulp.watch("src/html/*.html").on('change', browserSync.reload);
 });
 
-// 自动编译，再刷新浏览器
-gulp.task('watch', ['browserSync', 'sass', 'comcatjs'], function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    // Other watchers
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+
+
+gulp.task('default', ['serve']);
+
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+gulp.task('scripts', function() {
+    return gulp.src('src/javascript/*.js')
+        // .pipe(concat('all.js'))
+        .pipe(rev())
+        .pipe(gulp.dest('dist/javascript'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('dist/javascript'));
 });
 
-// 图片压缩
+
+gulp.task('styles', function() {
+    return gulp.src(['src/styles/*.css'])
+        .pipe(postcss(plugins))
+        .pipe(uglifycss({
+            "maxLineLen": 80,
+            "uglyComments": true
+          }))
+        .pipe(rev())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('dist/styles'))  
+});
+
+gulp.task('revCollectorCss', function () {
+    return gulp.src(['dist/**/*.json', 'dist/styles/*.css'])
+        .pipe(revCollector())
+        .pipe(gulp.dest('dist/styles/'));
+});
+
+
 gulp.task('images', function() {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
+    return gulp.src('src/images/*')
+        .pipe(rev())
         .pipe(gulp.dest('dist/images'))
-});
-
-// 给css加上版本号
-gulp.task('revcss', function() {
-    return gulp.src('app/css/*.css')
-        .pipe(concat('all.css'))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rev())
-        .pipe(gulp.dest('dist/css'))
         .pipe(rev.manifest())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/images'));
 });
-
-// 给all.js 加上版本号
-gulp.task('revjs', function() {
-    return gulp.src('app/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/build'))
-        .pipe(rev())
-        .pipe(gulp.dest('dist/js/build'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('dist/js/build'));
-});
-
-// 从lib的js里导入到dist
-gulp.task('linkjs', function() {
-    return gulp.src('app/js/lib/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/lib'));
-});
-
 gulp.task('rev', function() {
-    return gulp.src(['dist/**/*.json', 'app/*.html'])
+    return gulp.src(['dist/**/*.json', 'src/html/*.html'])
         .pipe(revCollector({
             replaceReved: true,
             dirReplacements: {
-                'css': 'css',
-                'js': 'js',
+                'styles': 'styles',
+                'javascript': 'javascript',
+                'images':'images',
                 'cdn': function(manifest_value) {
                     return '//cdn' + (Math.floor(Math.random() * 9) + 1) + '.' + 'exsample.dot' + '/img/' + manifest_value;
                 }
@@ -110,12 +88,11 @@ gulp.task('rev', function() {
             empty: true,
             spare: true
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/html'));
 });
 
-// (批量任务),上线之前build
 gulp.task('build', function(callback) {
-    runSequence('images', ['revcss', 'revjs', 'linkjs'],
+    runSequence('clean',['scripts','images','styles'], ['revCollectorCss'],
         'rev',
         callback);
 });
